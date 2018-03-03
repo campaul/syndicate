ecddd4fff25807a9cb43ebbd2704ba26af1ce00b
Cameron Paul <cpaul37@gmail.com>
2018-03-02 22:47:11 -0600

    Add skeleton with index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate hyper;
+extern crate spellbook;
+#[macro_use] extern crate tera;
+
+use std::rc::Rc;
+
+use spellbook::{
+    Request,
+    Response,
+    Result,
+    Router,
+    Spellbook
+};
+
+use tera::{
+    Tera,
+    Context,
+};
+
+#[derive(Clone)]
+struct Syndicate {
+    tera: Rc<Tera>,
+}
+
+fn index(app: Rc<Syndicate>, _req: Rc<Request>) -> Result {
+    let context = Context::new();
+    let body = try!(app.tera.render("index.html", &context));
+
+    Ok(Response::new()
+        .with_header(hyper::header::ContentLength(body.len() as u64))
+        .with_body(body))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = Syndicate {
+        tera: Rc::new(compile_templates!("templates/**/*")),
+    };
+
+    let router = Router::new()
+        .get("/", index);
+
+    Spellbook::new(app, router).serve("127.0.0.1:3000");
 }
